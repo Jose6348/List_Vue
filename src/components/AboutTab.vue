34e7639daa1d879900a3f7d9809d1862665c8e60
Jose6348 <jfalasco88@gmail.com>
2025-01-20 22:47:59 -0300

    New component

---
+++ src/components/AboutTab.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="tab-content">
+      <h2>Sobre Nós</h2>
+      <p>Aqui você encontra informações sobre nossa missão, visão e equipe.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutTab',
+  }
+  </script>
+  
+  <style scoped>
+  .tab-content {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+  
+  h2 {
+    color: #333;
+  }
+  
+  p {
+    color: #666;
+    line-height: 1.6;
+  }
+  </style>
